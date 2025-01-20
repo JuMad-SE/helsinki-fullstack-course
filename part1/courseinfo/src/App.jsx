79056fbc4c57d83dfe575319f2b64c08a6d63f19
JuMad-SE <jumadridc@unal.edu.co>
2025-01-20 17:17:47 -0500

    Improve code for use objetcts

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -31,28 +31,36 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Header course={course.name} />
+      <Content
+        part1={course.parts[0]}
+        part2={course.parts[1]}
+        part3={course.parts[2]}
+      />
       <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
+        exercises1={course.parts[0].exercises}
+        exercises2={course.parts[1].exercises}
+        exercises3={course.parts[2].exercises}
       />
     </div>
   );
