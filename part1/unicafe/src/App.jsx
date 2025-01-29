74940e61f9537e3834c1983df8992ee6aeb47ebc
JuMad-SE <jumadridc@unal.edu.co>
2025-01-28 19:28:42 -0500

    End exercise 1.6 Unicafe step1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+
 const Button = ({ text, onClick }) => <button onClick={onClick}>{text}</button>;
 
-const App = () => {
+const Statistics = ({ good, neutral, bad }) => {
   return (
-    <>
+    <div>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
+  return (
+    <div>
       <h1>give feedback</h1>
-      <Button text={"good"} />
-      <Button text={"neutral"} />
-      <Button text={"Bad"} />
-    </>
+      <Button text={"good"} onClick={handleGood} />
+      <Button text={"neutral"} onClick={handleNeutral} />
+      <Button text={"Bad"} onClick={handleBad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
   );
 };
 
