0961ee5b2d6582a71e90d2258db507d471dd015d
JuMad-SE <jumadridc@unal.edu.co>
2025-02-04 16:44:23 -0500

    Initialize component Course and refactorize props. Finally exercise 2.1 ends

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,32 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Part = ({ name, exercise }) => {
+  return (
+    <p>
+      {name} {exercise}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} name={part.name} exercise={part.exercises} />
+      ))}
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
